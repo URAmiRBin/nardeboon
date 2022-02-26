c12a3d93e9f9503afa4eacf52650bf84d432faf6
Amir Bin <ur.amirbin@gmail.com>
2022-02-26 15:45:12 +0330

    Set GameAnalytics keys on the runtime

--- Assets/Runner.cs
+++ Assets/Runner.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Runner : MonoBehaviour {
     // More analytics APIs might be added later
+    [Header("Game Analytics")]
     [SerializeField] bool useAnalytics;
     [SerializeField] GameObject gameAnalytics;
+    [SerializeField] string gameAnalyticsGameKey;
+    [SerializeField] string gameAnalyticsSecretKey;
 
     // Dependencies
     AnalyticsSystem analyticsSystem;
     
     void Awake() {
         if (useAnalytics) {
+            // Set GA settings
+            GameAnalyticsSDK.Setup.Settings gaSettings = Resources.Load<GameAnalyticsSDK.Setup.Settings>("GameAnalytics/Settings");
+            gaSettings.AddPlatform(RuntimePlatform.Android);
+            gaSettings.SetKeys(gameAnalyticsGameKey, gameAnalyticsSecretKey);
+            gaSettings.Build[0] = Application.version;
+            
+            // Initialize GA
             Instantiate(gameAnalytics);
             analyticsSystem = new GameAnalyticsSystem();
             analyticsSystem.Initialize();
