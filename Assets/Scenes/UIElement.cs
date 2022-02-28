1c76255601b6c8173f46045ff514489739e863e9
Amir Bin <ur.amirbin@gmail.com>
2022-02-28 12:46:13 +0330

    Block interactions when popup is up

--- Assets/Scenes/UIElement.cs
+++ Assets/Scenes/UIElement.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public abstract class UIElement : MonoBehaviour {
     public bool hasBackground;
-    public bool IsActive {get; private set;}
+    public bool IsActive {get; protected set;}
 
     public virtual void Open() {
         IsActive = true;
-        transform.GetChild(0).gameObject.SetActive(true);
+        foreach(Transform child in transform) child.gameObject.SetActive(true);
     }
     public virtual void Close() {
         IsActive = false;
-        transform.GetChild(0).gameObject.SetActive(false);
+        foreach(Transform child in transform) child.gameObject.SetActive(false);
     }
 }
