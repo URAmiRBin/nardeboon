1f4ff99edfd40a95c7bec2c9024343f0038dcd11
Amir Bin <ur.amirbin@gmail.com>
2022-03-07 15:17:53 +0330

    Reorganize advertisements folder

--- Assets/Advertisement/Examples/AdsExample.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class AdsExample : MonoBehaviour {
-    // void Start() => AdManager.Instance.InitializeAds();
-    public void ShowInterstitial() => AdManager.Instance.ShowInterstitial(null, null);
-    public void ShowRewarded() => AdManager.Instance.ShowRewarded(() => Debug.Log("Reward Gained"), () => Debug.Log("No Rewards"));
-    public void ShowBanner() => AdManager.Instance.ShowBanner();
-    public void HideBanner() => AdManager.Instance.DestroyBanner();
-}
