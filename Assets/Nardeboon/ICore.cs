12ed77310a7c418f3b189fad9bbc8d1164d931f2
Amir Bin <ur.amirbin@gmail.com>
2022-04-10 13:33:11 +0430

    Add ICore interface

---
+++ Assets/Nardeboon/ICore.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICore {
+    void StartGame();
+    void ExitGame();
+    void ReplayLevel();
+    void WinLevel();
+    void LoseLevel();
+    void Revive();
+    void FreezeGame();
+    void StartLevel(int level);
+    void RestartLevel();
+    void GetLevelReward();
+}
