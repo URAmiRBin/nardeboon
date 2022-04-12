21bc63b5700e2aecf92d6266bc65711e3c7c3db7
Amir Bin <ur.amirbin@gmail.com>
2022-04-12 19:14:48 +0430

    Remove scriptable objects as item configs

--- Assets/Nardeboon/Economy/ItemConfig.cs
+++ Assets/Nardeboon/Economy/ItemConfig.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "ItemConfig", menuName = "Nardeboon/ItemConfig", order = 1)]
-public class ItemConfig : ScriptableObject {
-    public new string name;
+[System.Serializable]
+public class ItemConfig {
+    public string name;
     public Sprite sprite;
     public int cost;
     [HideInInspector] public System.Action useCallback;
--- Assets/Nardeboon/Economy/PowerupConfig.cs
+++ Assets/Nardeboon/Economy/PowerupConfig.cs
@@ -1,9 +1,8 @@
 using UnityEngine;
 
-
-[CreateAssetMenu(fileName = "PowerupConfig", menuName = "Nardeboon/PowerupConfig", order = 2)]
-public class PowerupConfig : ScriptableObject {
-    public new string name;
+[System.Serializable]
+public class PowerupConfig {
+    public string name;
     public Sprite sprite;
     public int[] cost;
     [HideInInspector] public System.Action<int> useCallback;
