eec569a400f7fe3d2c7518ff2088363122d3ef83
Amir Bin <ur.amirbin@gmail.com>
2022-04-12 18:58:33 +0430

    Seperate save keys from preferences keys

--- Assets/Nardeboon/Utils/PlayerPrefKeys.cs
+++ Assets/Nardeboon/Utils/PlayerPrefKeys.cs
@@ -1,6 +1,10 @@
 public static class PlayerPrefKeys {
-    public readonly static string NOADS = "FOCKADS";
     public readonly static string VIBRATION = "EARTHQUAKE";
     public readonly static string AGREED = "VIRGIN";
+}
+
+public static class SaveKeys {
+    public readonly static string NOADS = "FOCKADS";
     public readonly static string COIN = "FLERBO";
+    public readonly static string LEVEL = "NOOBNESS";
 }
