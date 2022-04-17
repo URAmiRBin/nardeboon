10e99419c0c56493576e81ecb522ccc39a22fb9b
Amir Bin <ur.amirbin@gmail.com>
2022-04-17 11:58:37 +0430

    Setup a seperate storage structure for inventory save

--- Assets/Nardeboon/Economy/InventorySystem/Inventory.cs
+++ Assets/Nardeboon/Economy/InventorySystem/Inventory.cs
@@ -5,36 +5,51 @@ using UnityEngine;
 public class Inventory : MonoBehaviourSingletion<Inventory> {
     [SerializeField] GameItem currency;
     public List<InventoryItem> _items = new List<InventoryItem>();
+    List<InventoryItemStorageData> _storageItems;
     
     public int Wallet { get => _items[0].Amount; }
     public string MainCurrency { get => _items[0].Name; }
 
     void Awake() {
-        // TODO: Load
-        if (_items.Count == 0) _items.Add(new InventoryItem(currency));
+        _storageItems = ES3.Load(SaveKeys.INVENTORY, new List<InventoryItemStorageData>());
+        if (_items.Count == 0) AddToInventory(new InventoryItem(currency));
     }
 
     public void AddToInventory(InventoryItem item) {
         if (item == null || item.Price < 0) return;
         int index = _items.FindIndex(i => i.Name == item.Name);
-        if (index == -1) _items.Add(item);
-        else if (index == 0) AddToInventory(item.Amount);
-        else _items[index].Collect(item.Amount);
+        if (index == -1) {
+            _items.Add(item);
+            _storageItems.Add(new InventoryItemStorageData(item.Name, item.Amount));
+        }
+        else {
+            _items[index].Collect(item.Amount);
+            _storageItems[index] = _items[index].StorageData;
+        }
+        ES3.Save(SaveKeys.INVENTORY, _storageItems);
     }
 
     public bool HasItemWithName(string name) => _items.Exists(x => x.Name == name);
 
-    public void AddToInventory(int amount) {
+    public void AddToWallet(int amount) {
+        _items[0].Collect(amount);
         GameEvents.onCurrencyEarn?.Invoke(amount);
         Runner.UIManager.UpdateCoin();
-        _items[0].Collect(amount);
+        _storageItems[0] = _items[0].StorageData;
+        ES3.Save(SaveKeys.INVENTORY, _storageItems);
+    }
+
+    public void SpendFromWallet(int amount) {
+        _items[0].Use(amount);
+        GameEvents.onCurrencySpend?.Invoke(amount);
+        Runner.UIManager.UpdateCoin();
+        _storageItems[0] = _items[0].StorageData;
+        ES3.Save(SaveKeys.INVENTORY, _storageItems);
     }
 
     public void BuyItem(InventoryItem item) {
         try {
-            _items[0].Use(item.Price);
-            GameEvents.onCurrencySpend?.Invoke(item.Price);
-            Runner.UIManager.UpdateCoin();
+            SpendFromWallet(item.Price);
             AddToInventory(item);
         } catch (System.InvalidOperationException e) {
             throw e;
--- Assets/Nardeboon/Economy/InventorySystem/InventoryItem.cs
+++ Assets/Nardeboon/Economy/InventorySystem/InventoryItem.cs
@@ -9,29 +9,57 @@ public interface Collectable {
 public class InventoryItem : Collectable {
     [SerializeField] GameItem _item;
     [SerializeField] int amount;
+    InventoryItemStorageData _storageData;
 
     public int Amount { get => amount; }
     public string Name { get => _item.name; }
     public Sprite Sprite { get => _item.image; }
     public int Price { get => _item.cost * (IsConsumable ? amount : 1); }
     public bool IsConsumable { get => _item.consumable; }
+    public InventoryItemStorageData StorageData => _storageData;
     public GameItem Config { get => _item; }
 
-    public InventoryItem(GameItem item, int amount = 0) {
+    public InventoryItem(GameItem item, int amount = 1000) {
         _item = item;
         this.amount = amount;
+        _storageData = new InventoryItemStorageData(Name, Amount);
     }
 
-    public void Collect(int amount = 1) => this.amount += amount;
+    public void Collect(int amount = 1) {
+        this.amount += amount;
+        _storageData.Amount = Amount;
+    }
 
     public void Use(int amount = 1) {
         if (amount > this.amount) throw new System.InvalidOperationException();
         this.amount -= amount;
         _item.Use();
+        _storageData.Amount = Amount;
     }
 }
 
 [System.Serializable]
 public enum GameItemValue {
     Common, Rare, Epic
+}
+
+[System.Serializable]
+public class InventoryItemStorageData {
+    public string _name;
+    public int _amount;
+
+    public int Amount { 
+        get => _amount;
+        set => _amount = value; 
+    }
+
+    public InventoryItemStorageData(string name, int amount) {
+        _name = name;
+        _amount = amount;
+    }
+
+    public InventoryItemStorageData() {
+        _name = "";
+        _amount = 0;
+    }
 }
