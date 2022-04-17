a646931c48218b0fc15b361ff3797b74a94d17ce
Amir Bin <ur.amirbin@gmail.com>
2022-04-17 12:30:29 +0430

    Implement simple resource loader

---
+++ Assets/Nardeboon/Economy/InventorySystem/ResourceLoader.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: Connect resource loader to loading screen
+
+public static class ResourceLoader {
+    private static readonly string itemsPath = "Items/";
+    public static T LoadData<T>(string path) where T : UnityEngine.Object {
+        return Resources.Load<T>(path);
+    }
+
+    public static GameItem LoadItem(string name) => LoadData<GameItem>(itemsPath + name);
+}
