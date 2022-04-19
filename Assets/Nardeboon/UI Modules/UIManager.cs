68ccb3437697762b1084698a8be903f6fb0fe5c1
Amir Bin <ur.amirbin@gmail.com>
2022-04-19 18:36:11 +0430

    Make UI Manager not singleton

--- Assets/Nardeboon/UI Modules/UIManager.cs
+++ Assets/Nardeboon/UI Modules/UIManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIManager : MonoBehaviourSingletion<UIManager> {
+public class UIManager : MonoBehaviour {
     [SerializeField] GameStates defaultState;
     [SerializeField] UIMaps maps;
     [SerializeField] GameObject backgroundPanel;
