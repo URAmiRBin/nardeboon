96d9072f123253022960da9bc2dc9eee1964f283
Amir Bin <ur.amirbin@gmail.com>
2022-04-19 17:19:14 +0430

    Make noAdsButton not interactable if noads is purchased

--- Assets/Nardeboon/UI Modules/SettingsPanel.cs
+++ Assets/Nardeboon/UI Modules/SettingsPanel.cs
@@ -22,7 +22,7 @@ public class SettingsPanel : UIElement {
         SetVibrationState(_vibrationState);
         _soundState = PlayerPrefs.GetInt(PlayerPrefKeys.SOUND) == 1 ? true : false;
         SetSoundState(_soundState);
-        // TODO: NoAds
+        noAdsButton.interactable = !ES3.Load(SaveKeys.NOADS, false);
     }
 
     void Start() {
